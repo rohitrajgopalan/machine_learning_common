12ec9c1a2ac6bf0572e98eb1147875bfd5d36f93
Rohit Gopalan <rohitgopalan1990@gmail.com>
2020-06-29 14:27:41 +0930

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ai-ml-common',
+      version='0.1',
+      description='A Basic setup for a common library for AI and ML',
+      author='Rohit Gopalan',
+      author_email='rohitgopalan1990@gmail.com',
+      license='MIT',
+      packages=['neural_network', 'reinforcement_learning', 'supervised_learning'],
+      zip_safe=False)
