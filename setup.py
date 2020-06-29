4e266a5f4202e28361401e5c4d1f446689071d29
Rohit Gopalan <rohitgopalan1990@gmail.com>
2020-06-29 14:36:00 +0930

    Changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ai-ml-common',
-      version='0.1',
+      version='1.0',
       description='A Basic setup for a common library for AI and ML',
       author='Rohit Gopalan',
       author_email='rohitgopalan1990@gmail.com',
