383bafcfc80a17ac1a2335d62726458ff992a9d8
Rohit Gopalan <rohitgopalan1990@gmail.com>
2020-07-10 11:59:00 +0930

    Add work around if we say encounter an empty array.

--- reinforcement_learning/policy/softmax.py
+++ reinforcement_learning/policy/softmax.py
@@ -16,32 +16,38 @@ class Softmax(Policy):
             A 2D array of shape (batch_size, num_actions). Where each column is a probability distribution over
             the actions representing the policy.
         """
-        # Compute the preferences by dividing the action-values by the temperature parameter tau
-        preferences = action_values / self.tau
-        # Compute the maximum preference across the actions
-        max_preference = np.max(preferences, axis=1, keepdims=True)
+        try:
+            # Compute the preferences by dividing the action-values by the temperature parameter tau
+            preferences = action_values / self.tau
+            # Compute the maximum preference across the actions
+            max_preference = np.max(preferences, axis=1, keepdims=True)
 
-        # Reshape max_preference array which has shape [Batch,] to [Batch, 1]. This allows NumPy broadcasting 
-        # when subtracting the maximum preference from the preference of each action.
-        reshaped_max_preference = max_preference.reshape((-1, 1))
+            # Reshape max_preference array which has shape [Batch,] to [Batch, 1]. This allows NumPy broadcasting
+            # when subtracting the maximum preference from the preference of each action.
+            reshaped_max_preference = max_preference.reshape((-1, 1))
 
-        # Compute the numerator, i.e., the exponential of the preference - the max preference.
-        exp_preferences = np.exp(preferences - reshaped_max_preference)
-        # Compute the denominator, i.e., the sum over the numerator along the actions axis.
-        sum_of_exp_preferences = np.sum(exp_preferences, axis=1, keepdims=True)
+            # Compute the numerator, i.e., the exponential of the preference - the max preference.
+            exp_preferences = np.exp(preferences - reshaped_max_preference)
+            # Compute the denominator, i.e., the sum over the numerator along the actions axis.
+            sum_of_exp_preferences = np.sum(exp_preferences, axis=1, keepdims=True)
 
-        # Reshape sum_of_exp_preferences array which has shape [Batch,] to [Batch, 1] to  allow for NumPy broadcasting 
-        # when dividing the numerator by the denominator.
-        reshaped_sum_of_exp_preferences = sum_of_exp_preferences.reshape((-1, 1))
+            # Reshape sum_of_exp_preferences array which has shape [Batch,] to [Batch, 1] to  allow for NumPy broadcasting
+            # when dividing the numerator by the denominator.
+            reshaped_sum_of_exp_preferences = sum_of_exp_preferences.reshape((-1, 1))
 
-        # Compute the action probabilities according to the equation in the previous cell.
-        action_probs = exp_preferences / reshaped_sum_of_exp_preferences
+            # Compute the action probabilities according to the equation in the previous cell.
+            action_probs = exp_preferences / reshaped_sum_of_exp_preferences
 
-        # squeeze() removes any singleton dimensions. It is used here because this function is used in the 
-        # agent policy when selecting an action (for which the batch dimension is 1.) As np.random.choice is used in 
-        # the agent policy and it expects 1D arrays, we need to remove this singleton batch dimension.
-        action_probs = action_probs.squeeze()
-        return action_probs
+            # squeeze() removes any singleton dimensions. It is used here because this function is used in the
+            # agent policy when selecting an action (for which the batch dimension is 1.) As np.random.choice is used in
+            # the agent policy and it expects 1D arrays, we need to remove this singleton batch dimension.
+            action_probs = action_probs.squeeze()
+            return action_probs
+        except ValueError:
+            preferences = np.zeros((1, self.num_actions)) / self.tau
+            exp_preferences = np.exp(preferences)
+            sum_of_exp_preferences = np.sum(exp_preferences, axis=1, keepdims=True)
+            return exp_preferences / sum_of_exp_preferences
 
     def choose_action_based_from_values(self, action_values):
         probs_batch = self.derive_policy_based_from_values(action_values)
