Refix Softmax policy to flatten probability array. Also, attempt to fix memory issues when writing CSVs to file.

reinforcement_learning/agent/agent.py
@@ -144,19 +144,21 @@ class Agent:
                     else:
                         action = str(df['ACTION'])
                 else:
-                    action_as_list = row['ACTION']
+                    action_as_list = row['ACTION'].split(';')
+                    actions = []
                     for a in action_as_list:
                         try:
                             if '.' in a:
                                 a = float(a)
                             else:
                                 a = int(a)
+                            actions.append(a)
                         except TypeError:
                             continue
                     if action_type == 'tuple':
-                        action = tuple(action_as_list)
+                        action = tuple(actions)
                     else:
-                        action = np.array([action_as_list])
+                        action = np.array([actions])
             else:
                 action = row['ACTION']
             actions_from_csv.append(action)

reinforcement_learning/policy/softmax.py
@@ -8,13 +8,20 @@ class Softmax(Policy):
     tau = 0.0
 
     def derive_policy_based_from_values(self, action_values):
-        action_probs = np.exp(action_values/self.tau)
-        sum_probs = np.sum(action_probs)
-        return action_probs/sum_probs
+        if self.num_actions == 1:
+            return 1
+        else:
+            action_probs = np.exp(action_values/self.tau)
+            sum_probs = np.sum(action_probs)
+            policy = action_probs/sum_probs
+            try:
+                return policy.flatten()
+            except TypeError:
+                return policy.reshape((1, self.num_actions))
 
     def choose_action_based_from_values(self, action_values):
-        probs_batch = self.derive_policy_based_from_values(action_values)
-        try:
-            return self.rand_generator.choice(self.num_actions, p=probs_batch.squeeze()) if self.num_actions > 1 else 0
-        except TypeError:
-            return self.rand_generator.choice(self.num_actions, p=probs_batch) if self.num_actions > 1 else 0
+        policy = self.derive_policy_based_from_values(action_values)
+        if self.num_actions == 1:
+            return 0
+        else:
+            return self.rand_generator.choice(self.num_actions, p=policy)

reinforcement_learning/policy/static.py
@@ -33,11 +33,13 @@ class StaticPolicy(Policy):
                         val = int(val)
                     state_as_list.append(val)
                 state = np.array([state_as_list])
-            action = int(row['ACTION'])
-            if state in self.policy_table:
-                self.policy_table[state].append(action)
-            else:
-                self.policy_table.update({state: [action]})
+            actions = row['ACTION(S)'].split(';')
+            if state not in self.policy_table:
+                self.policy_table[state] = []
+            actions = [int(action) for action in actions]
+            for action in actions:
+                if action not in self.policy_table[state]:
+                    self.policy_table[state].append(action)
 
     def derive(self, state, network):
         if type(state) == tuple: