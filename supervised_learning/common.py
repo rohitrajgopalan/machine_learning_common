d53b0e371a111c5db74e711f5fdf50dce0d542af
Rohit Gopalan <rohitgopalan@hotmail.com>
2020-07-15 13:57:38 +0930

    Update common.py

--- supervised_learning/common.py
+++ supervised_learning/common.py
@@ -225,6 +225,25 @@ def select_best_classifier(csv_dir, best_type='', metric='Accuracy', features=[]
 def select_best_regressor(csv_dir, best_type='', metric='Accuracy', features=[], label='', filters={}):
     return select_best_method(csv_dir, regressors, best_type, metric, features, label, filters, MethodType.Regression)
 
+def select_classifier(csv_dir, choosing_method='best', features=[], label='', filters={}):
+    if choosing_method == 'best':
+        return select_best_classifier(csv_dir=csv_dir,features=features,label=label,filters=filters)
+    elif choosing_method == 'random':
+        historical_data = load_from_directory(csv_dir, cols, filters, concat=True)
+        return randomly_select_classifier(), historical_data
+    elif choosing_method in classifiers:
+        historical_data = load_from_directory(csv_dir, cols, filters, concat=True)
+        return classifiers[choosing_method], historical_data
+        
+def select_regressor(csv_dir, choosing_method='best', features=[], label='', filters={}):
+    if choosing_method == 'best':
+        return select_best_regressor(csv_dir=csv_dir,features=features,label=label,filters=filters)
+    elif choosing_method == 'random':
+        historical_data = load_from_directory(csv_dir, cols, filters, concat=True)
+        return randomly_select_regressor(), historical_data
+    elif choosing_method in regressors:
+        historical_data = load_from_directory(csv_dir, cols, filters, concat=True)
+        return regressors[choosing_method], historical_data
 
 def randomly_select_method(methods):
     key, val = random.choice(list(methods.items()))
